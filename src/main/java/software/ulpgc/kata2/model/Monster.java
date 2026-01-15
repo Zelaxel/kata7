232285a136ef1f1a054d9e90af1e3a57189a0c9c
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-01-15 01:42:38 +0000

    Monster class added.

---
+++ src/main/java/software/ulpgc/kata2/model/Monster.java
@@ -0,0 +1,13 @@
+package software.ulpgc.kata2.model;
+
+import java.util.List;
+
+public record Monster(String name, Type type, boolean isLarge, List<String> games) {
+    public enum Type {
+        Leviathan, Brute_Wyvern, Neopteron, Fanged_Beast, Flying_Wyvern,
+        Bird_Wyvern, Elder_Dragon, Relict, Herbivore, Fanged_Wyvern,
+        Wingdrake, Piscine_Wyvern, Amphibian, Lynian, Carapaceon,
+        unknown, Fish, Demi_Elder, Construct, Temnoceran,
+        Relicts, Snake_Wyvern, Cephalopod
+    }
+}
